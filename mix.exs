35b7df8edb6b11da8c4e3817f1285bb24763e93e
Thomas Schmitt <thomas.schmitt@ni.com>
2019-07-11 01:05:08 -0700

    Release new version 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TDMS.Parser.MixProject do
   def project do
     [
       app: :tdms_parser,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
